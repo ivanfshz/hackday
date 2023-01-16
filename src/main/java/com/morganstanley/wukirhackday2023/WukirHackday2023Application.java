54e3f08acf19b068d7431a237ec57d55bca354f4
Kira Wu <kira.wu@morganstanley.com>
2023-01-16 11:42:20 -0500

    add github worflow for terraform and sample spring boot project in intellij

---
+++ src/main/java/com/morganstanley/wukirhackday2023/WukirHackday2023Application.java
@@ -0,0 +1,13 @@
+package com.morganstanley.wukirhackday2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WukirHackday2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WukirHackday2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/morganstanley/wukirhackday2023/WukirHackday2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morganstanley.wukirhackday2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WukirHackday2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
